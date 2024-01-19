8dfe140970aab4f6adaeff9c7ed636c11a6de6fb
clay.tyler <ctyler9@me.com>
2024-01-19 15:51:52 -0600

    random tests

--- src/main.c
+++ src/main.c
@@ -1,5 +1,20 @@
-int _main() {
-  // Your code goes here
+#include "idt.h"
+#include "isr.h"
+#include "screen.h"
+#include "util.h"
 
-  return 0;
+void _main() {
+  // Your code goes here
+  idt_init();
+  isr_init();
+  screen_init();
 }
+
+// void _main() {
+//   unsigned short *VGA_MEMORY = (unsigned short *)(0xA0000);
+//   VGA_MEMORY[0] = 0x3333;
+//   VGA_MEMORY[1] = 0x4444;
+//   VGA_MEMORY[293] = 0x4444;
+//   for (;;) {
+//   }
+// }
